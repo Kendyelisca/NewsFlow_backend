b7376cb8b0021115dfa508255e26c8b157f4b47e
Kendy <kendyelisca5@gmail.com>
2023-11-24 11:06:12 -0500

    added user table

---
+++ src/models/User.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../utils/connection");
+
+const User = sequelize.define("user", {
+  Name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = User;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mailRouter = require("./mail.router");
 const storyRouter = require("./story.router");
+const userRouter = require("./user.router");
 const router = express.Router();
 
 // colocar las rutas aquí
 router.use("/mail", mailRouter);
 router.use("/stories", storyRouter);
+router.use("/users", userRouter);
 module.exports = router;
