c9ccb6b33fef4bb6ed6fafd1d15756922bc6ca99
Kendy <kendyelisca5@gmail.com>
2023-11-24 12:24:41 -0500

    login endpoint added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,7 @@
 const catchError = require("../utils/catchError");
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const getAll = catchError(async (req, res) => {
   const results = await User.findAll();
@@ -7,7 +9,8 @@ const getAll = catchError(async (req, res) => {
 });
 
 const create = catchError(async (req, res) => {
-  const result = await User.create(req.body);
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  const result = await User.create({ ...req.body, password: hashedPassword });
   return res.status(201).json(result);
 });
 
@@ -34,10 +37,26 @@ const update = catchError(async (req, res) => {
   return res.json(result[1][0]);
 });
 
+const login = catchError(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ where: { email } });
+  if (!user) return res.status(401).json({ error: "invalid credentials" });
+
+  const isValid = await bcrypt.compare(password, user.password);
+  if (!isValid) return res.status(401).json({ error: "invalid credentials" });
+
+  const token = jwt.sign({ user }, process.env.TOKEN_SECRET, {
+    expiresIn: "1d",
+  });
+
+  return res.json({ user, token });
+});
+
 module.exports = {
   getAll,
   create,
   getOne,
   remove,
   update,
+  login,
 };
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -4,13 +4,21 @@ const {
   getOne,
   remove,
   update,
+  login,
 } = require("../controllers/user.controller");
 const express = require("express");
+const verifyJWT = require("../utils/verifyJWT");
 
 const userRouter = express.Router();
 
-userRouter.route("/").get(getAll).post(create);
+userRouter.route("/").get(verifyJWT, getAll).post(create);
 
-userRouter.route("/:id").get(getOne).delete(remove).put(update);
+userRouter
+  .route("/:id")
+  .get(verifyJWT, getOne)
+  .delete(verifyJWT, remove)
+  .put(verifyJWT, update);
+
+userRouter.route("/login").post(login);
 
 module.exports = userRouter;
