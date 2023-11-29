f8628e1978615ef6bc000d009fca85112e4cddb9
Kendy <kendyelisca5@gmail.com>
2023-11-29 15:52:57 -0500

    added send email functionality

--- src/controllers/mail.controller.js
+++ src/controllers/mail.controller.js
@@ -1,5 +1,6 @@
 const catchError = require("../utils/catchError");
 const Mail = require("../models/Mail");
+const sendEmail = require("../utils/sendEmail");
 
 const getAll = catchError(async (req, res) => {
   const results = await Mail.findAll();
@@ -8,6 +9,58 @@ const getAll = catchError(async (req, res) => {
 
 const create = catchError(async (req, res) => {
   const result = await Mail.create(req.body);
+  const { email } = req.body;
+
+  const mailOptions = {
+    to: email,
+    subject: "Welcome to NewsFlow's Newsletter!",
+    html: `
+      <html>
+        <head>
+          <style>
+            body {
+              font-family: 'Arial', sans-serif;
+              background-color: #f4f4f4;
+            }
+            .container {
+              max-width: 600px;
+              margin: 20px auto;
+              padding: 20px;
+              background-color: #fff;
+              border-radius: 10px;
+              box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            }
+            h1 {
+              color: #333;
+            }
+            p {
+              color: #666;
+            }
+          </style>
+        </head>
+        <body>
+          <div class="container">
+            <h1>Welcome to our Newsletter!</h1>
+            <p>
+              Thank you for subscribing. We're excited to share our latest news and updates with you.
+            </p>
+            <p>Best regards,<br>Your Newsletter Team</p>
+          </div>
+        </body>
+      </html>
+    `,
+  };
+
+  // Send the email using the utility
+  try {
+    await sendEmail(mailOptions);
+    console.log("Welcome email sent successfully");
+  } catch (error) {
+    console.error("Error sending welcome email:", error);
+    // Handle error
+    return res.status(500).json({ error: "Failed to send welcome email" });
+  }
+
   return res.status(201).json(result);
 });
 
