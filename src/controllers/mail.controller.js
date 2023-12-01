dedb12619d46735cb98f40c87b6d78544d163882
Kendy <kendyelisca5@gmail.com>
2023-12-01 10:38:18 -0500

    added automatic email news update with node-cron

--- src/controllers/mail.controller.js
+++ src/controllers/mail.controller.js
@@ -1,10 +1,16 @@
 const catchError = require("../utils/catchError");
 const Mail = require("../models/Mail");
 const sendEmail = require("../utils/sendEmail");
+const axios = require("axios");
+const cron = require("node-cron");
 
 const getAll = catchError(async (req, res) => {
   const results = await Mail.findAll();
-  return res.json(results);
+  if (res) {
+    return res.json(results);
+  } else {
+    console.error("Response object is undefined.");
+  }
 });
 
 const create = catchError(async (req, res) => {
@@ -76,6 +82,116 @@ const create = catchError(async (req, res) => {
 
   return res.status(201).json(result);
 });
+
+const fetchAndSendNewsletter = async () => {
+  const apiKey = process.env.API_KEY;
+  try {
+    // Step 2: Fetch latest news from GNews API
+    const newsResponse = await axios.get(
+      `https://gnews.io/api/v4/top-headlines?category=general&lang=en&apikey=${apiKey}`
+    );
+    const latestNews = newsResponse.data.articles.slice(0, 5);
+
+    // Step 3: Retrieve subscribed users
+    const subscribedUsers = await Mail.findAll();
+
+    // Step 4: Prepare email options array
+    const emailOptionsArray = subscribedUsers.map((mail) => ({
+      to: mail.email,
+      subject: "Latest News Update from NewsFlow",
+      html: composeNewsletterEmail(mail.email, latestNews),
+    }));
+
+    // Step 5: Send emails in parallel
+    await Promise.all(emailOptionsArray.map(sendEmail));
+
+    console.log("Newsletter sent to all subscribed users");
+  } catch (error) {
+    console.error("Error fetching news or sending newsletters:", error);
+  }
+};
+
+const composeNewsletterEmail = (userName, latestNews) => {
+  // Compose the email content with the latest news
+  const newsList = latestNews.map((article) => {
+    return `
+      <div style="margin-bottom: 20px;">
+        <h2 style="color: #333;">${article.title}</h2>
+        <p style="color: #666;">${article.description}</p>
+        <p style="color: #666; font-style: italic;">Published on: ${new Date(
+          article.publishedAt
+        ).toLocaleString()}</p>
+        <a href="${article.url}" style="color: #ff0000;">Read more</a>
+      </div>
+    `;
+  });
+
+  return `
+    <html>
+      <head>
+        <style>
+          body {
+            font-family: 'Arial', sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+          }
+
+          .container {
+            max-width: 600px;
+            margin: 20px auto;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+          }
+
+          h1 {
+            color: #ff0000; /* Red Title */
+          }
+
+          p {
+            color: #666;
+            line-height: 1.5;
+          }
+
+          a {
+            color: #ff0000;
+            text-decoration: none;
+          }
+
+          .btn {
+            display: inline-block;
+            padding: 10px 20px;
+            margin-top: 20px;
+            background-color: #808080;
+            color:  #ffffff;
+            text-decoration: none;
+            border-radius: 5px;
+          }
+
+          .newsletter-team {
+            margin-top: 20px;
+            color: #333;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="container">
+          <h1>Hello ${userName}!</h1>
+          <p>Here are the latest updates from NewsFlow:</p>
+          ${newsList.join("")}
+          <p class="newsletter-team">
+            Enjoy staying informed with NewsFlow!<br>Your NewsFlow Team
+          </p>
+          <a href="https://news-flow-two.vercel.app/" class="btn">Go to NewsFlow App</a>
+        </div>
+      </body>
+    </html>
+  `;
+};
+
+cron.schedule("0 5 * * *", fetchAndSendNewsletter);
 
 const getOne = catchError(async (req, res) => {
   const { id } = req.params;
